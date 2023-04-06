95b1abe8dfac2600b782770d8dccf7a4c13e6ece
navyabd <navya.bade@gmail.com>
2023-04-06 19:37:11 -0400

    first commit

--- student.py
+++ student.py
@@ -1,1 +1,10 @@
 import math
+
+def list_courses(id, c_roster):
+    if id in c_roster:
+        courses = c_roster[id]
+        print(f"Student {id} is registered for the following courses:")
+        for course in courses:
+            print(course)
+    else:
+        print(f"Student {id} is not registered for any courses.")

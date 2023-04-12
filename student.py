8a0ca00d8713e319098ac91ef874a5451e3e9de4
KongStomper <115477275+KongStomper@users.noreply.github.com>
2023-04-12 18:51:43 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,11 +1,41 @@
-import math
+def add_course(id, c_roster, c_max_size, registered_courses, course, waitlist):
+    if course not in c_roster:
+        print("Course not found")
+        return
+    if id in c_roster[course]:
+        print("You are already enrolled in that course.")
+        return
+    if len(c_roster[course]) >= c_max_size[course]:
+        print("Course is full. Adding to waitlist...")
+        waitlist.append((id, course))
+        print(f"{id} added to waitlist for {course}")
+        return
+    c_roster[course].append(id)
+    registered_courses.append(course)
+    print(f"{id} added to {course}")
+
+
+def drop_course(id, c_roster, registered_courses, waitlist, course, c_max_size):
+    if course not in c_roster:
+        print("Course not found")
+        return
+    if id not in c_roster[course]:
+        print("You are not enrolled in that course.")
+        return
+    c_roster[course].remove(id)
+    registered_courses.remove(course)
+    print(f"{id} dropped from {course}")
+    if waitlist:
+        next_student, next_course = waitlist.pop(0)
+        add_course(next_student, c_roster, c_max_size, registered_courses, next_course, waitlist)
+        print(f"{next_student} from waitlist added to {next_course}")
 
 def list_courses(id, c_roster):
-    if id in c_roster:
-        courses = c_roster[id]
+    registred_courses = []
+    for course, students in c_roster.items():
+        for id in students:
+            registered_courses.append(course)
         print(f"Student {id} is registered for the following courses:")
-        for course in courses:
-            print(course)
-    else:
-        print(f"Student {id} is not registered for any courses.")
-##
+        for course in registered_courses
+            print(registered_courses)
+        print(f"Total number: {len(registered_courses)}")

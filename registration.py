dadc53980fdd04449c942db9620060b1b12dc1c8
KongStomper <115477275+KongStomper@users.noreply.github.com>
2023-04-24 13:58:54 -0400

    Update registration.py

--- registration.py
+++ registration.py
@@ -2,12 +2,12 @@ from student import add_course, drop_course, list_courses, calculate_class_and_s
 from billing import calculate_hours_and_bill, display_hours_and_bill
 
 # Student data
+student_list = [('1001', '111'), ('1002', '222'), ('1003', '333'), ('1004', '444')]
 student_in_state = {'1001': True, '1002': False, '1003': True, '1004': False}
 course_hours = {'CSC101': 3, 'CSC102': 4, 'CSC103': 5, 'CSC104': 3}
 course_roster = {'CSC101': ['1004', '1003'], 'CSC102': ['1001'], 'CSC103': ['1002'], 'CSC104': []}
 course_max_size = {'CSC101': 3, 'CSC102': 2, 'CSC103': 1, 'CSC104': 3}
-
-###LOGIN FUNCTION+ Encryption for pin HERE#####
+student_gpa = [('1001', 3.5), ('1002', 2.2), ('1003', 4.0), ('1004', 3.9)]
 import hashlib
 
 def encrypt_pin(pin):
@@ -33,8 +33,6 @@ def login(id, s_list):
 student_list = [('1001', encrypt_pin('111')), ('1002', encrypt_pin('222')), ('1003', encrypt_pin('333')), ('1004', encrypt_pin('444'))]
 
   
-
-###########################
 def main():
     """Manage course registration system."""
     s_list = dict(student_list)
@@ -53,6 +51,7 @@ def main():
                     f"\n|{'4: Show Bill':^20s}|\n" + " -------- ".center(20) +
                     f"\n|{'5: Show GPA:':^20s}|\n" + " -------- ".center(20) +
                     f"\n|{'0 to exit:':^20s}|\n" + " -------- ".center(20))
+
                 choice = input()
                 if choice == '0':
                     break
@@ -71,12 +70,13 @@ def main():
                         print(f"Added {id} to waitlist for {course}.")
                 elif choice == '2':
                     course = input("Enter course you want to drop: ")
+                    # Check if the currents student is registered to the course they want to drop
                     if course not in course_hours:
                         print("Course not found")
                     elif id not in course_roster[course]:
                         print("You are not enrolled in that course.")
                     else:
-                        drop_course(id, course_roster, registered_courses, waitlist, course, c_max_size)
+                        drop_course(id, course_roster, registered_courses, waitlist, course, course_max_size)
                         print("Course dropped.")
                         # Check if there are students on the waitlist for this course
                         if waitlist:
@@ -90,5 +90,6 @@ def main():
                     display_hours_and_bill(credit_hours, cost)
                 elif choice == '5':
                     calculate_class_and_student_gpa(course_roster, student_gpa, id)
+
 if __name__ == '__main__':
     main()

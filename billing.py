668ea74138d64975c5be270cb06e528f8c5d6922
KongStomper <115477275+KongStomper@users.noreply.github.com>
2023-04-24 12:31:03 -0400

    Update billing.py

--- billing.py
+++ billing.py
@@ -1,13 +1,19 @@
-def calculate_billing_info(id, s_in_state, c_rosters, c_hours):
-    hours = 0
-    cost = 0
-    for course in c_rosters:
-        if id in c_rosters[course]:
-            hours += c_hours.get(course, 0)
-            if s_in_state:
-                cost += c_hours[course] * 225
+def calculate_hours_and_bill(id, s_in_state, c_rosters, c_hours):
+    in_state = s_in_state.get(id, False)
+    total_hours = 0
+    total_cost = 0
+    for course, roster in c_rosters.items():
+        if id in roster:
+            hours = c_hours[course]
+            total_hours += hours
+            if in_state:
+                cost = hours * 225
             else:
-                cost += c_hours[course] * 850
-        return hours, cost
+                cost = hours * 850
+            total_cost += cost
+    return total_hours, total_cost
+
 
 def display_hours_and_bill(hours, cost):
+    print(f"Total credit hours: {hours}")
+    print(f"Total cost of enrollment: ${cost}")
